a898c294003f82ee362a4497946b8150418e789c
srichandra <ch.srichandra@gmail.com>
2016-05-31 15:12:59 +0530

    code for multiplication of two unsigned 8-bit numbers

---
+++ mult.v
@@ -0,0 +1,40 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date:    14:38:56 04/16/2016 
+// Design Name: 
+// Module Name:    mult 
+// Project Name: 
+// Target Devices: 
+// Tool versions: 
+// Description: 
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
+module mult(multiplier,multiplicand, product); //P
+   input [7:0]  multiplier, multiplicand;
+   output        product;
+
+   reg [15:0]    product;
+
+   integer       i;
+
+   always @( multiplier or multiplicand )
+     begin
+
+        product = 0;
+
+        for (i=0; i<8; i=i+1)
+          if ( multiplier[i] == 1'b1 )
+            product = product + ( multiplicand << i );
+
+     end
+
+endmodule
